e8cb4638cff321934b2e2f16ede3656038d13c1e
Jackson Behan <jb681831@gmail.com>
2016-08-05 14:02:28 -0700

    Edit, Update, and Destroy actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,6 +3,7 @@ class PicsController < ApplicationController
 
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
@@ -20,6 +21,22 @@ class PicsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Your pic was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
   private
